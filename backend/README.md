3d18cb13c150f7698b89faea70b1498ba52557b3
manav <manavkhadka2004@gmail.com>
2025-03-22 21:20:06 +0530

    [removed] delete unused files and configurations across multiple directories

--- backend/README.md
+++
@@ -1,3 +0,0 @@
-﻿# dockapi-backend
-
-## This contain 3 repo 1. build-server 2. dockapi-main-server  3. s3-reverse-proxy
--- src/app/actions/signout.tsx
+++
@@ -1,7 +0,0 @@
-'use server';
-import { signOut } from "../../auth";
-
-export async function signOutFromEverywhere() {
-    
-        await signOut() 
-}
